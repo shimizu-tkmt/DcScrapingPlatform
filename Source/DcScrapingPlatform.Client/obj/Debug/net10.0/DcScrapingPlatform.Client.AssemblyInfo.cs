b40306f1822dfea4d1fc1e0eba114098ce488eee
omochis-antigravity <shimizu.tkmt+omochis-antigravity@gmail.com>
2026-03-16 00:00:38 +0900

    feat: Issue #4 DB接続設定の最適化 (User Secrets と Neon Branching)

--- Source/DcScrapingPlatform.Client/obj/Debug/net10.0/DcScrapingPlatform.Client.AssemblyInfo.cs
+++ Source/DcScrapingPlatform.Client/obj/Debug/net10.0/DcScrapingPlatform.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DcScrapingPlatform.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda9e568c5ebaca5e38ead84199b4b6d99f3aac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d471cb8d8f023ce3f574b0b20abeb380e15d40")]
 [assembly: System.Reflection.AssemblyProductAttribute("DcScrapingPlatform.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DcScrapingPlatform.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
